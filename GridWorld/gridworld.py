12120a708c9513cf05eddd139c74b919d0b9e8e5
Nguyn Lai <ng.lam1412@gmail.com>
2026-09-21 21:40:16 +0700

    refactor(env): refactor GridWorld environment to OOP structure

--- GridWorld/gridworld.py
+++ GridWorld/gridworld.py
@@ -1,54 +1,57 @@
 import numpy as np
 
-walls = [[1, 1], [2, 3]]
-lakes = [[3, 1]]
-exit = (4, 4)
+class GridWorldEnv():
+    def __init__(self, width = 5, height = 5):
+        self.width = width
+        self.height = height
+        self.state = (0, 0)
+        self.walls = {(1, 1), (2, 3)}
+        self.lakes = {(3, 1)}
+        self.exit = (4, 4)
 
-state = (0, 0)
+        self.action_ids = (0, 1, 2, 3)
+        self.action = {0: (1, 0),  # up
+                       1: (-1, 0), # down
+                       2: (0, -1), # left
+                       3: (0, 1)}  # right
 
-action_ids = (0, 1, 2, 3)
-actions = {0: (1, 0),  # up
-           1: (-1, 0), # down
-           2: (0, -1), # left
-           3: (0, 1)}  # right
-probs = [0.25, 0.25, 0.25, 0.25]
+        self.stoc_policy = (0, -1, 1) # correct, slide left, slide right
+        self.stoc_probs = (0.8, 0.1, 0.1)
 
-stoc_policy = [0,  # good
-               -1, # left
-               1]  # right
-stoc_policy_probs = [0.8, 0.1, 0.1]
-gamma = 0.95   
-reward = 0
+        self.gamma = 0.95
 
-def step(s, a):
-    global reward
-    dx, dy = a
-    x, y = s
-    nx, ny = (x + dx, y + dy)
-    ns = (nx, ny)
-    if ns in walls or ns in lakes: 
-        reward -= 1
-        return False
-    if nx < 0 or nx > 4 or ny < 0 or ny > 4: 
-        return False
-    return True
+    def reset(self):
+        self.state = (0, 0)
+        return self.state 
 
-slide = 0
-for i in range(100000):
-    reward -= 0.04
-    action_idx = np.random.choice(action_ids, p = probs)
-    stoc = np.random.choice(stoc_policy, p = stoc_policy_probs)
-    if stoc == 0: 
-        dx, dy = actions[action_idx]
-        if(step(state, (dx, dy))):
-            state = (state[0] + dx, state[1] + dy)
-    elif (step(state, (0, stoc))):
-        state = (state[0], state[1] + stoc)
+    def step(self, a):
+        reward = 0
+        reward -= 0.04
+        done = 0
+        dx, dy = a 
+        stoc = np.random.choice(self.stoc_policy, p = self.stoc_probs)
+        x, y = self.state
+        if stoc != 0: dx, dy = (0, 0)
+        nx, ny = (x + dx, y + dy + stoc)
+        next_state = (nx, ny)
+        if nx < 0 or nx >= self.width  or ny < 0 or ny >= self.height or next_state in self.walls:
+            next_state = self.state 
+        if next_state in self.lakes:
+            reward -= 1
+            done = 1
+        elif next_state == self.exit:
+            reward += 1
+            done = 1
 
-    if stoc != 0: slide += 1
+        self.state = next_state
+        return next_state, reward, done
 
-    if (state == 'exit'):
-        print(f"Found the exit with the reward of {reward:.2f} at step {i}")
-        break
-    
-print(slide/100000)
+env = GridWorldEnv()
+count = 0
+for i in range(1000):
+    env.reset()
+    env.step((0, 1))
+    if env.state == (0, 1): 
+        count += 1
+
+print(f"Run 1000 times with {count / 10:.2f}% go to (0, 1)")
